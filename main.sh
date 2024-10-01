dcdb7ba89585ab88d9a930ca9c72fd7f2b12a115
DevevolperPlus <yuncopylocked@gmail.com>
2024-10-01 21:04:14 +0000

    Windows Configuring

---
+++ main.sh
@@ -0,0 +1,8 @@
+#npm run fetch
+npm run webpack:compile
+#npm run webpack:watch
+#npm run
+#npm run electron:package:dir
+#npm run electron:start
+#npm run webpack:prod4
+npx electron-builder --windows nsis --x64
